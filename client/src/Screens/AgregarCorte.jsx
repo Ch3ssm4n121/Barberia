c5d954f341bc4d6bf5e3b9c5b770fdbe3ae496cc
Ch3ssm4n121 <jesus14hp@hotmail.com>
2025-04-29 18:10:04 -0700

    modificacion de registro de cortes

--- client/src/Screens/AgregarCorte.jsx
+++ client/src/Screens/AgregarCorte.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 import './AgregarCorte.css';
+
 const AgregarCorte = () => {
   const [formData, setFormData] = useState({
     nombre: '',
@@ -7,7 +9,19 @@ const AgregarCorte = () => {
     precio: '',
     imagen: null,
   });
-
+  const [cortes, setCortes] = useState([]);
+  useEffect(() => {
+    const obtenerCortes = async () => {
+      try {
+        const res = await axios.get('http://localhost:3001/api/cortes');
+        setCortes(res.data);
+      } catch (error) {
+        console.error('Error al obtener cortes:', error);
+      }
+    };
+  
+    obtenerCortes();
+  }, []);
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     setFormData({
@@ -58,7 +72,24 @@ const AgregarCorte = () => {
         <input type="file" name="imagen" accept="image/*" onChange={handleChange} />
         <button type="submit">Agregar Corte</button>
       </form>
-      
+      <div className="cortes-lista">
+  {cortes.length === 0 ? (
+    <p>No hay cortes registrados.</p>
+  ) : (
+    cortes.map((corte) => (
+      <div key={corte.id} className="corte-card">
+        <img
+          src={`http://localhost:3001/uploads/${corte.imagen}`}
+          alt={corte.nombre}
+          className="corte-img"
+        />
+        <h3>{corte.nombre}</h3>
+        <p><strong>Precio:</strong> ${corte.precio}</p>
+        <p>{corte.descripcion}</p>
+      </div>
+    ))
+  )}
+</div>
     </div>
   );
 };
